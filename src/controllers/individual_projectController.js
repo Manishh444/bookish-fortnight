1db54e10b5b4722e02989f4c3967b57ac8a96fa9
Bhardwaj <manish.b.bhardwaj@capgemini.com>
2023-08-28 22:37:50 +0530

    try catch error

--- src/controllers/individual_projectController.js
+++ src/controllers/individual_projectController.js
@@ -2,14 +2,24 @@ const pool = require("../config/dbConfig");
 
 // Create a new project
 async function userParticipatingInGroupProject(userId) {
-  //   const checkUserQuery = "SELECT * FROM user_group_project WHERE user_id = $1";
-  const checkUserQuery = `SELECT users.*, user_group_project.group_project_id
-    FROM users
-    RIGHT JOIN user_group_project ON users.user_id = user_group_project.user_id 
-    WHERE users.user_id = $1;`;
-  const result = await pool.query(checkUserQuery, [userId]);
-  // return result.rows[0];
-  return result.rows.length > 0 ? result.rows[0] : null;
+  // const checkUserQuery = `SELECT users.*, user_group_project.group_project_id
+  //                             FROM users
+  //                             RIGHT JOIN user_group_project ON users.user_id = user_group_project.user_id 
+  //                             WHERE users.user_id = $1;`;
+  //     const result = await pool.query(checkUserQuery, [userId]);
+  //     // return result.rows[0];
+  //     return result.rows.length > 0 ? result.rows[0] : null;
+  try {
+      const checkUserQuery = `SELECT users.*, user_group_project.group_project_id
+                              FROM users
+                              RIGHT JOIN user_group_project ON users.user_id = user_group_project.user_id
+                              WHERE users.user_id = $1;`;
+      const result = await pool.query(checkUserQuery, [userId]);
+      // return result.rows[0];
+      return result.rows.length > 0 ? result.rows[0] : null;
+  } catch(error) {
+    console.log(error)
+  }
 }
 const createProject = async (req, res) => {
   try {
